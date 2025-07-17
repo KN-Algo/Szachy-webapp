0184ccb7c99732d76020ca1c2e4eb2d804a53897
debovyy <debowy.eu@gmail.com>
2025-07-17 19:49:36 +0200

    Zmiany wyglądowe
    Obramówki grubsze i zaokrąglone, tekst większy i czcionka zmienona, box z logami powiększony i przyciski są teraz obok siebie, dodana poświata do obramówek.

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Magiczne szachy</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
@@ -19,13 +22,14 @@
     <div id="interfejs">
         <div id="interfejsDziecko" class="bg-black bg-gradient">
             <div id="sterowanie">
-                <div>
-                    <button id="mikrofon" class="btn btn-outline-primary">
+                <div id="obaPrzyciskiKontener">
+                    <button id="mikrofon" class="btn btn-outline-primary border-3">
                         <img src="./img/mikrofon.png" alt="mikrofon">
                     </button>
-                    <button id="cofnijRuch" class="btn btn-outline-danger">
+                    <button id="cofnijRuch" class="btn btn-outline-danger border-3">
                         <img src="./img/cofnij.png" alt="confij">
                     </button>
+                </div>
                     <form id="wprowadzRecznie">
                         <h2>Wprowadź ruch ręcznie</h2>
 
@@ -37,7 +41,7 @@
                             <input type="reset" value="Anuluj" class="btn btn-outline-danger przyciski">
                         </div>
                     </form>
-                </div>
+
             </div>
             <div id="logi">
                 <h2>Logi ruchów</h2>
